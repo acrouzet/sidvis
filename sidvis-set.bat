238aa0dbe53f1e6467e09e438df3946e459e9b36
Acro <47704566+acrouzet@users.noreply.github.com>
2024-10-31 15:12:21 -0400

    Change sidvis-set.bat defaults to a lighter 6581 curve

--- sidvis-set.bat
+++ sidvis-set.bat
@@ -1,15 +1,15 @@
-set sidplayfp_path=sidvis-sidplayfp
+set sidplayfp_path=
 
 set hvsc_path=
 
-set ffmpeg_path=C:\Users\aidan\OneDrive\Desktop\Stuff\Replace\ffmpeg
+set ffmpeg_path=
 
-set wav_path=C:\Users\aidan\.jsidplay2
+set wav_path=
 
 
 set use_hvsc=0
 
-set sid_path=C:\Users\aidan\OneDrive\Desktop\GTUltra\win32\61.sid
+set sid_path=
 
 set track=1
 
@@ -30,7 +30,7 @@ set sid_model=6581
 set combined_waves=average
 
 set filter_curve_6581=1.0
-set filter_range_6581=0.4
+set filter_range_6581=0.5
 
 
 set delay=4096
